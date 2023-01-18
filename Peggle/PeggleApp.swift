15b7d34d5937d77b2c1d78c6a02fdeefc47184e0
petermonky <wjdgustjr0554@gmail.com>
2023-01-18 22:37:09 +0800

    Initial Commit

---
+++ Peggle/PeggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeggleApp.swift
+//  Peggle
+//
+//  Created by Peter Jung on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct PeggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
